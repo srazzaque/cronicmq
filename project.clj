2b54e82cc8ed04b82555c1ebdb181ed5ce6d74da
Sandipan Razzaque <me@sandipan.net>
2014-03-25 18:09:33 -0400

    Increment version in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.cronic/zeromq-clj "0.1.1-SNAPSHOT"
+(defproject io.cronic/zeromq-clj "0.1.2-SNAPSHOT"
   :description "Clojure-friendly access to ZeroMQ (using the JeroMQ implementation)"
   :url "http://cronic.io/zeromq-clj"
   :license {:name "Eclipse Public License"
