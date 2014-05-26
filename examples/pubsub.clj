126b633c381192fa20c083e2b59d60dd4a894ef9
Sandipan Razzaque <me@sandipan.net>
2014-05-26 17:52:58 -0400

    Updated pub/sub example for new API.

--- examples/pubsub.clj
+++ examples/pubsub.clj
@@ -1,21 +1,24 @@
-(require '[zeromq-clj.core :as zmq] :reload-all)
+(require '[zeromq-clj.core :as zmq])
 
-(do 
-  (def ctx (zmq/create-context))
-  (def sub (zmq/sub-socket ctx "tcp://127.0.0.1:5858" "myTopic"))
-  (def pub (zmq/pub-socket ctx "tcp://127.0.0.1:5858"))
-  (def ex (java.util.concurrent.Executors/newFixedThreadPool 5))
-  (def received-messages (atom [])))
+(require '[clojure.core.async :as a])
 
-(def disruptor (zmq/on-msg sub
-                           (fn [m]
-                             (swap! received-messages conj m))
-                           ex))
+(def subscription (zmq/subscription "tcp://127.0.0.1:5858/myTopic"))
+
+(def publisher (zmq/publisher "tcp://127.0.0.1:5858"))
+
+(def received-messages (atom []))
+
+(def done? (promise))
 
 (try
-  (dotimes [n 1000]
-    (zmq/publish pub {:event n :message "Hello there!"} "myTopic"))
+  (let [c (a/chan)]
+    (a/go
+     (while (not (= 1000
+                    (count @received-messages)))
+       (swap! received-messages conj (subscription)))
+     (deliver done? true))
+    (dotimes [n 1000]
+      (publisher {:event n :message "Hello there!"} "myTopic")))
   (finally
-    (.shutdownNow ex)
-    (.shutdown disruptor)
-    (zmq/close ctx)))
+    @done?
+    (zmq/close! :all)))
